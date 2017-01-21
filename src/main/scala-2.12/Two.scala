5e0bbe588149ef717ebc6f811e960e3e3be357cd
Harsha Sri-Narayana <harshavsn@gmail.com>
2017-01-21 11:37:50 +0000

    2). Refactor, clarity and efficiency

--- src/main/scala-2.12/Two.scala
+++ src/main/scala-2.12/Two.scala
@@ -1,25 +1,22 @@
+import scala.collection.immutable.::
+
 class Two {
   def penultimate(xs: List[Int]): Int = {
-    if (xs == null) throw new IllegalArgumentException("Cannot penultimate null")
-    if (xs.isEmpty) throw new IllegalArgumentException("Cannot penultimate empty list")
-    if (xs.tail.isEmpty) throw new IllegalArgumentException("Cannot penultimate list of 1")
     def _penultimate(xs: List[Int]): Int = {
       if (xs.tail.tail.isEmpty) xs.head
       else _penultimate(xs.tail)
     }
 
-    def _penultimateWithPatternMatching(xs: List[Int]): Int = {
-      xs match {
-        case null => throw new IllegalArgumentException("Cannot penultimate null")
-        case Nil => throw new IllegalArgumentException("Cannot penultimate empty list")
-        case head :: Nil => throw new IllegalArgumentException("Cannot penultimate list of 1")
-        case head :: second :: Nil => head
-        case head :: second :: tail  => _penultimateWithPatternMatching(tail)
-      }
+    def _penultimateWithPatternMatching(xs: List[Int]): Int = xs match {
+      case head :: second :: Nil => head
+      case head :: second :: tail => _penultimateWithPatternMatching(tail)
     }
+
+    if (xs == null) throw new IllegalArgumentException("Cannot penultimate null")
+    if (xs == Nil) throw new IllegalArgumentException("Cannot penultimate empty list")
+    if (xs.tail == Nil) throw new IllegalArgumentException("Cannot penultimate list of 1")
+
     _penultimateWithPatternMatching(xs)
-//    _penultimate(xs)
+    //    _penultimate(xs)
   }
-
-
 }
