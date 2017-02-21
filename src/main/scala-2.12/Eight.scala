f2474afab06c71ce95bb1f9546f6ca6852872a8e
Harsha Sri-Narayana <harshavsn@gmail.com>
2017-02-21 20:13:21 +0000

    Solve 8

---
+++ src/main/scala-2.12/Eight.scala
@@ -0,0 +1,25 @@
+class Eight {
+  def removeDuplicates(xs: List[Symbol]): List[Symbol] = {
+    def _notMatchedState(remaining: List[Symbol], resultSoFar: List[Symbol]): List[Symbol] = {
+      remaining match {
+        case Nil => resultSoFar
+        case last :: Nil => resultSoFar :+ last
+        case first :: second :: tail =>
+          if (first == second) _matchedState(tail, resultSoFar :+ first, first)
+          else _notMatchedState(second :: tail, resultSoFar :+ first)
+      }
+    }
+
+    def _matchedState(remaining: List[Symbol], resultSoFar: List[Symbol], matchedElem: Symbol): List[Symbol] = {
+      remaining match {
+        case Nil => resultSoFar
+        case last :: Nil => resultSoFar
+        case head :: tail =>
+          if (head == matchedElem) _matchedState(tail, resultSoFar, matchedElem)
+          else _notMatchedState(head :: tail, resultSoFar)
+      }
+    }
+
+    _notMatchedState(xs, Nil)
+  }
+}
---
+++ src/test/scala/EightTest.scala
@@ -0,0 +1,21 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class EightTest extends FlatSpec with Matchers {
+  "Eight " can " remove duplicates in a list" in {
+    new Eight().removeDuplicates(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should
+      be(List('a, 'b, 'c, 'a, 'd, 'e))
+  }
+
+  "Eight " can " do nothing to list which has no duplicates" in {
+    new Eight().removeDuplicates(List('a, 'b, 'c, 'd, 'e)) should
+      be(List('a, 'b, 'c, 'd, 'e))
+  }
+
+  "Eight " can " do nothing to empty list" in {
+    new Eight().removeDuplicates(Nil) should be(Nil)
+  }
+
+  "Eight " can " do nothing to list with single element" in {
+    new Eight().removeDuplicates(List('a)) should be(List('a))
+  }
+}
